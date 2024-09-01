64b7338e1f9ae265b0df0bf1ca36aea83f972b38
Martini <166950628+martinosalasm@users.noreply.github.com>
2024-09-01 16:33:06 -0400

    cambio a la version actualizada

---
+++ Proyecto-QR/src/app/Access/perfil/perfil.page.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-perfil',
+  templateUrl: './perfil.page.html',
+  styleUrls: ['./perfil.page.scss'],
+})
+export class PerfilPage implements OnInit {
+  username = '';
+  constructor(private router: Router) {
+    const navegacion = this.router.getCurrentNavigation();
+    const state = navegacion?.extras.state as {
+      username: '';
+      password: '';
+      nombre: '';
+      apellido:'';
+      fdn:''
+    };
+    this.username = state.username;
+    //Console.log
+    //Mensaje bienvenida
+  }
+
+  ngOnInit() {}
+}
